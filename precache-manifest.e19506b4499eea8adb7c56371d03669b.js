8038565c644068e4a01c48fa90da91d17475d430
Bowen Zhang <bowenzhang@gmail.com>
2020-11-29 19:13:12 -0500

    Updates

---
+++ precache-manifest.e19506b4499eea8adb7c56371d03669b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a27c1b7017686c4fb19bc9b4e6493c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c8c8800beb0546e831a",
+    "url": "/static/css/main.f86cf579.chunk.css"
+  },
+  {
+    "revision": "4955a00ddcdd422bbc77",
+    "url": "/static/js/2.968b4e97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.968b4e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8c8800beb0546e831a",
+    "url": "/static/js/main.88abd582.chunk.js"
+  },
+  {
+    "revision": "6a1ab118d7a69edc78c1",
+    "url": "/static/js/runtime-main.da6e5043.js"
+  }
+]);
