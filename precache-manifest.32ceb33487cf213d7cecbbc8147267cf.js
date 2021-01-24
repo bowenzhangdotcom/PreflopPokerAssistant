fb3f2b3b40b653c03942f176d2523ba6dcece8a8
Bowen Zhang <bowenzhang@gmail.com>
2021-01-23 20:57:15 -0500

    Updates

---
+++ precache-manifest.32ceb33487cf213d7cecbbc8147267cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2989ce47d9a5f4f1ba087d7a4541197",
+    "url": "/PreflopPokerAssistant/index.html"
+  },
+  {
+    "revision": "5ff9a66953593ea65a6e",
+    "url": "/PreflopPokerAssistant/static/css/main.497fb2d9.chunk.css"
+  },
+  {
+    "revision": "4955a00ddcdd422bbc77",
+    "url": "/PreflopPokerAssistant/static/js/2.968b4e97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PreflopPokerAssistant/static/js/2.968b4e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ff9a66953593ea65a6e",
+    "url": "/PreflopPokerAssistant/static/js/main.4f3b7f26.chunk.js"
+  },
+  {
+    "revision": "0ecc491d735fee76b02e",
+    "url": "/PreflopPokerAssistant/static/js/runtime-main.11107ad2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/preflop-poker-local/precache-manifest.ab6f4a288cd9e9dd6e78241aa22b8887.js"
+  "/PreflopPokerAssistant/precache-manifest.32ceb33487cf213d7cecbbc8147267cf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/preflop-poker-local/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PreflopPokerAssistant/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
